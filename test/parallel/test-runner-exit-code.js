a2cfd7a904bc4dd95f58480d291310b6b0b181e5
Julian Gruber <julian@juliangruber.com>
2022-03-26 11:20:25 +0100

    port parallel tests

---
+++ test/parallel/test-runner-exit-code.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// https://github.com/cjihrig/node/blob/527eb5caa5feae3b748d9c5b74b256edbb40a775/test/parallel/test-runner-exit-code.js
+
+const assert = require('assert')
+const { spawnSync } = require('child_process')
+
+if (process.argv[2] === 'child') {
+  const test = require('../..')
+
+  if (process.argv[3] === 'pass') {
+    test('passing test', () => {
+      assert.strictEqual(true, true)
+    })
+  } else {
+    assert.strictEqual(process.argv[3], 'fail')
+    test('failing test', () => {
+      assert.strictEqual(true, false)
+    })
+  }
+} else {
+  let child = spawnSync(process.execPath, [__filename, 'child', 'pass'])
+  assert.strictEqual(child.status, 0)
+  assert.strictEqual(child.signal, null)
+
+  child = spawnSync(process.execPath, [__filename, 'child', 'fail'])
+  assert.strictEqual(child.status, 1)
+  assert.strictEqual(child.signal, null)
+}
