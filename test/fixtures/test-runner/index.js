803caec42d06d6504c2ace21a841f272d0cc744a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-04-22 00:43:40 +0200

    fixup! Update nodejs/node HEAD sha

--- test/fixtures/test-runner/index.js
+++ test/fixtures/test-runner/index.js
@@ -1,2 +1,3 @@
+// https://github.com/nodejs/node/blob/1aab13cad9c800f4121c1d35b554b78c1b17bdbd/test/fixtures/test-runner/index.js
 'use strict'
 throw new Error('thrown from index.js')
--- test/fixtures/test-runner/index.test.js
+++ test/fixtures/test-runner/index.test.js
@@ -1,3 +1,4 @@
+// https://github.com/nodejs/node/blob/1aab13cad9c800f4121c1d35b554b78c1b17bdbd/test/fixtures/test-runner/index.test.js
 'use strict'
 const test = require('#node:test')
 
