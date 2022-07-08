826048cc350c9f5a92e0681b50d0659241806b0f
Moshe Atlow <moshe@atlow.co.il>
2022-07-08 13:17:44 +0300

    feat: cancel on termination
    PR-URL: https://github.com/nodejs/node/pull/43549
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Nitzan Uziely <linkgoron@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/parallel/test-runner-exit-code.js
+++ test/parallel/test-runner-exit-code.js
@@ -1,10 +1,12 @@
-// https://github.com/nodejs/node/blob/1aab13cad9c800f4121c1d35b554b78c1b17bdbd/test/parallel/test-runner-exit-code.js
+// https://github.com/nodejs/node/blob/1523a1817ed9b06fb51c0149451f9ea31bd2756e/test/parallel/test-runner-exit-code.js
 
 'use strict'
 
-require('../common')
+const common = require('../common')
 const assert = require('assert')
 const { spawnSync } = require('child_process')
+const { promisify } = require('util')
+const setTimeout = promisify(require('timers').setTimeout)
 
 if (process.argv[2] === 'child') {
   const test = require('#node:test')
@@ -13,12 +15,18 @@ if (process.argv[2] === 'child') {
     test('passing test', () => {
       assert.strictEqual(true, true)
     })
-  } else {
+  } else if (process.argv[3] === 'fail') {
     assert.strictEqual(process.argv[3], 'fail')
     test('failing test', () => {
       assert.strictEqual(true, false)
     })
-  }
+  } else if (process.argv[3] === 'never_ends') {
+    assert.strictEqual(process.argv[3], 'never_ends')
+    test('never ending test', () => {
+      return setTimeout(100_000_000)
+    })
+    process.kill(process.pid, 'SIGINT')
+  } else assert.fail('unreachable')
 } else {
   let child = spawnSync(process.execPath, [__filename, 'child', 'pass'])
   assert.strictEqual(child.status, 0)
@@ -27,4 +35,15 @@ if (process.argv[2] === 'child') {
   child = spawnSync(process.execPath, [__filename, 'child', 'fail'])
   assert.strictEqual(child.status, 1)
   assert.strictEqual(child.signal, null)
+
+  child = spawnSync(process.execPath, [__filename, 'child', 'never_ends'])
+  assert.strictEqual(child.status, 1)
+  assert.strictEqual(child.signal, null)
+  if (common.isWindows) {
+    common.printSkipMessage('signals are not supported in windows')
+  } else {
+    const stdout = child.stdout.toString()
+    assert.match(stdout, /not ok 1 - never ending test/)
+    assert.match(stdout, /# cancelled 1/)
+  }
 }
