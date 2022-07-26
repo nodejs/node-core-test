f875da2bd834f710d4bde40f77bf848f8700de20
Antoine du Hamel <duhamelantoine1995@gmail.com>
2022-07-27 00:37:41 +0200

    feat: validate `concurrency` option
    PR-URL: https://github.com/nodejs/node/pull/43976
    Reviewed-By: Darshan Sen <raisinten@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    (cherry picked from commit 60da0a1b364efdd84870269d23b39faa12fb46d8)

--- test/parallel/test-runner-option-validation.js
+++ test/parallel/test-runner-option-validation.js
@@ -1,4 +1,4 @@
-// https://github.com/nodejs/node/blob/d83446b4c4694322e12d2b7d22592f2be674e580/test/parallel/test-runner-option-validation.js
+// https://github.com/nodejs/node/blob/60da0a1b364efdd84870269d23b39faa12fb46d8/test/parallel/test-runner-option-validation.js
 
 'use strict'
 require('../common')
@@ -15,4 +15,16 @@ const test = require('node:test');
 [null, undefined, Infinity, 0, 1, 1.1].forEach((timeout) => {
   // Valid values should not throw.
   test({ timeout })
+});
+
+// eslint-disable-next-line symbol-description
+[Symbol(), {}, [], () => {}, 1n, '1'].forEach((concurrency) => {
+  assert.throws(() => test({ concurrency }), { code: 'ERR_INVALID_ARG_TYPE' })
+});
+[-1, 0, 1.1, -Infinity, NaN, 2 ** 33, Number.MAX_SAFE_INTEGER].forEach((concurrency) => {
+  assert.throws(() => test({ concurrency }), { code: 'ERR_OUT_OF_RANGE' })
+});
+[null, undefined, 1, 2 ** 31, true, false].forEach((concurrency) => {
+  // Valid values should not throw.
+  test({ concurrency })
 })
