2f02171b589e19aaf427f9a1ea2efda719ef11b7
Moshe Atlow <moshe@atlow.co.il>
2022-07-31 14:53:06 +0300

    fix: empty pending tests queue post running
    PR-URL: https://github.com/nodejs/node/pull/44059
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    (cherry picked from commit 5ec2d7bc5deed26ac640feff279800e39dacc9c0)

--- test/message/test_runner_unresolved_promise.js
+++ test/message/test_runner_unresolved_promise.js
@@ -1,4 +1,4 @@
-// https://github.com/nodejs/node/blob/1aab13cad9c800f4121c1d35b554b78c1b17bdbd/test/message/test_runner_unresolved_promise.js
+// https://github.com/nodejs/node/blob/5ec2d7bc5deed26ac640feff279800e39dacc9c0/test/message/test_runner_unresolved_promise.js
 // Flags: --no-warnings
 'use strict'
 require('../common')
@@ -6,4 +6,4 @@ const test = require('#node:test')
 
 test('pass')
 test('never resolving promise', () => new Promise(() => {}))
-test('fail')
+test('fail', () => console.log('this should not appear'))
