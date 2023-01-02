29c73b4fdf571062a80262667748d10b6015caf9
Moshe Atlow <moshe@atlow.co.il>
2023-01-02 23:22:54 +0200

    feat: report `file` in test runner events
    PR-URL: https://github.com/nodejs/node/pull/46030
    Reviewed-By: Benjamin Gruenbaum <benjamingr@gmail.com>
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    (cherry picked from commit 9eb363a3e00dbba572756c7ed314273f17ea8e2e)

--- test/fixtures/test-runner/custom_reporters/custom.js
+++ test/fixtures/test-runner/custom_reporters/custom.js
@@ -1,7 +1,12 @@
-// https://github.com/nodejs/node/blob/a1b27b25bb01aadd3fd2714e4b136db11b7eb85a/test/fixtures/test-runner/custom_reporters/custom.js
+// https://github.com/nodejs/node/blob/9eb363a3e00dbba572756c7ed314273f17ea8e2e/test/fixtures/test-runner/custom_reporters/custom.js
+const assert = require('assert')
+const path = require('path')
 module.exports = async function * customReporter (source) {
   const counters = {}
   for await (const event of source) {
+    if (event.data.file) {
+      assert.strictEqual(event.data.file, path.resolve(__dirname, '../reporters.js'))
+    }
     counters[event.type] = (counters[event.type] ?? 0) + 1
   }
   yield 'custom.js '
